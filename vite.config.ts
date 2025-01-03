6d4c9dcbd19c64cadf92918f89a6e9896fa94bcb
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-03 13:23:43 +0700

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scoring-system-prototuype/",
+  base: "/scoring-system-prototype/",
 
   resolve: {
     alias: {
