6d35b761a5d128151f586d7aebfb4a8253c3c8e1
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-03 13:26:06 +0700

    Update root path

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: "/scoring-system-prototype",
         element: <ScoringPrototypePage />,
       },
     ],
