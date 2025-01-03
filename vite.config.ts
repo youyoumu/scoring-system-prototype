3eb48e08bb24d13ee4fbb6cb8b3463ac6c68d064
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-03 13:21:43 +0700

    Update base vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<REPO>/",
+  base: "/scoring-system-prototuype/",
 
   resolve: {
     alias: {
